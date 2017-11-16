22a53cd115b1bdd1830ee552dcd378431a6ed396
Willford <willfordzhan@gmail.com>
2017-11-17 00:45:09 +0800

    amended bugs in bio

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,11 +4,11 @@ This is empty on purpose! Your code to build the resume will go here.
  var bio = {
    name:"Willford",
    role:"Student",
-   contact_info:["1506676573@qq.com",],
-   picture_url:,
+   contact_info:["1506676573@qq.com"],
+   picture_url:"none",
    welcome_message:"Hi, welcome to my resume",
    skills:"table tennis, tennis and HTML, CSS"
  }
 
-$("#main").append(bio.role);
-$("#main").append(bio.name);
+$('#main').append(bio.role);
+$('#main').append(bio.name);
