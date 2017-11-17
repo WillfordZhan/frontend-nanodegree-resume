b2332faa869a14b9be321bd8fdd904d0f7f8dd02
WillfordZhan <willfordzhan@gmail.com>
2017-11-17 19:22:45 +0800

    begin to add work, project, education, bio

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,15 +2,24 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
- var bio = {
-   name: "Willford",
-   role:"student",
-   contact_info:"1506676573",
-   pic_url:"none",
-   welcome_message:"Hi, jipsis",
-   skills:"table tennis"
+/* var bio = {
+   "name": "Willford",
+   "role":"student",
+   "contact_info":"1506676573",
+   "pic_url":"none",
+   "welcome_message":"Hi, jipsis",
+   "skills":"table tennis"
  };
 
 $("#main").append(bio.name);
 $("#main").append(bio.role);
 $("#main").append(bio.welcome_message);
+*/
+var work = {};
+var education = {};
+
+work["position"] = "tester";
+education.name = "ZJUT";
+
+$("#main").append(work["position"]);
+$("#main").append(education.name);
