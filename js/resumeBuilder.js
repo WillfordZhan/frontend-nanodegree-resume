824686ad7882c908b1c975227bca18a593866511
WillfordZhan <willfordzhan@gmail.com>
2017-11-17 16:04:37 +0800

    added bio and appended it

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,16 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ var bio = {
+   name: "Willford",
+   role:"student",
+   contact_info:"1506676573",
+   pic_url:"none",
+   welcome_message:"Hi, jipsis",
+   skills:"table tennis"
+ };
+
+$("#main").append(bio.name);
+$("#main").append(bio.role);
+$("#main").append(bio.welcome_message);
